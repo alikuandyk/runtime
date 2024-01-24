b6160382b7cff8c03008409c68b05c9dace4af31
Kuandyk Mukhamedali <alikuandyk26@gmail.com>
2024-01-24 19:54:38 +0600

    Версия 6

--- src/lesson15/classwork/task2/FirstType.java
+++ src/lesson15/classwork/task2/FirstType.java
@@ -6,8 +6,8 @@ public class FirstType extends TaxType{
     public BigDecimal calculateTaxFor(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.valueOf(100_000)) == -1) {
             return amount.divide(BigDecimal.valueOf(0.1));
-        } else {
-            return amount.divide(BigDecimal.valueOf(0.15));
         }
+        return amount.divide(BigDecimal.valueOf(0.15));
+
     }
 }
